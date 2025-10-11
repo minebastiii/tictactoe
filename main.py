2de380696930002d73c575acba70664a8ee29107
1Link2U <erik.otto3110@gmail.com>
2025-10-11 14:14:19 +0200

    Print_Board_With_LineBreaks

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+board = ["[ ]" for _ in range(9)]
+
+for x, board_tile in enumerate((board), 1):
+    if x % 3 == 0:
+        print(board_tile, "\n")
+    else:
+        print(board_tile, end=" ")
