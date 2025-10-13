f10a2d7d548766743a74e27f8924d78a31bcfef1
1Link2U <erik.otto3110@gmail.com>
2025-10-13 16:54:45 +0200

    Print_Board_Basci_Input

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-board = ["[ ]" for _ in range(9)]
+def create_board():
 
-for x, board_tile in enumerate((board), 1):
-    if x % 3 == 0:
-        print(board_tile, "\n")
-    else:
-        print(board_tile, end=" ")
+    board = [str(x+1) for x in range(9)]
+    
+    #board[2]= "X"
+    #board[4]= "[O]"
+    return board
+
+def print_board(board):
+
+    for x, board_tile in enumerate((board), 1):
+        if x % 3 == 0:
+            print(f"[{board_tile}]", "\n")
+        else:
+            print(f"[{board_tile}]", end=" ")
+
+
+def player_action(board):
+
+    print("Player 1 it's your turn")
+    
+    action = input("Enter a Number:")
+    board[int(action)-1] = "X"
+    
+    print_board(board)
+
+board = create_board()
+print_board(board)
+player_action(board)
